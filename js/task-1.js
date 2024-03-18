4010beca2f7ba0042989ba5d439769f3a071182c
sergiifedorishin <151446351+sergiy-fedorishin@users.noreply.github.com>
2024-03-18 21:16:04 +0200

    tasc1

--- js/task-1.js
+++ js/task-1.js
@@ -1,37 +1,15 @@
-// function slugify(title) {
-//   title = title.toLowerCase().replace(/\s+/g, '-');
+function isEnoughCapacity(products, containerSize) {
+  let totalQuantity = 0;
+  for (const quantity of Object.values(products)) {
+    totalQuantity += quantity;
+  }
+  return totalQuantity <= containerSize;
+}
 
-//   return `${title}`;
-// }
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
 
-// console.log(slugify('Arrays for begginers'));
-// console.log(slugify('English for developer'));
-// console.log(slugify('Ten secrets of JavaScript'));
-// console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
 
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
 
-const apartment = {
-  imgUrl: 'https://via.placeholder.com/640x480',
-  descr: 'Spacious apartment in the city center',
-  rating: 4,
-  price: 2153,
-  tags: ['premium', 'promoted', 'top'],
-  owner: {
-    name: 'Henry',
-    phone: '982-126-1588',
-    email: 'henry.carter@aptmail.com',
-  },
-};
-
-const ownerName = apartment.owner.name;
-const ownerPhone = apartment.owner.phone;
-const ownerEmail = apartment.owner.email;
-const numberOfTags = apartment.tags.length;
-const firstTag = apartment.tags[0];
-const lastTag = apartment.tags[2];
-console.log(ownerName);
-console.log(ownerPhone);
-console.log(ownerEmail);
-console.log(numberOfTags);
-console.log(firstTag);
-console.log(lastTag);
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
--- js/task-2.js
+++ js/task-2.js
@@ -1,15 +1,30 @@
-function makeArray(firstArray, secondArray, maxLength) {
-  let allArray = firstArray.concat(secondArray);
-  if (allArray.length > maxLength) {
-    return allArray.slice(0, maxLength);
-  } else {
-    return allArray;
-  }
-}
+// Напиши функцію calcAverageCalories(days), яка повертає середньодобове значення кількості калорій, які спортсмен споживав
+// протягом тижня.Функція очікує один параметр: days — масив об’єктів.Кожен об’єкт описує день тижня та кількість калорій calories,
+//   спожитих спортсменом, у цей день.Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+//   У консоль будуть виведені результати її викликів.
 
-console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4));
-console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3));
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2));
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4));
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0));
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 3010 },
+    { day: 'tuesday', calories: 3200 },
+    { day: 'wednesday', calories: 3120 },
+    { day: 'thursday', calories: 2900 },
+    { day: 'friday', calories: 3450 },
+    { day: 'saturday', calories: 3280 },
+    { day: 'sunday', calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
