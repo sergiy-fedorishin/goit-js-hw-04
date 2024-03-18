082b2e6cb7d543a11906f47b5364a84a554d4c9c
sergiifedorishin <151446351+sergiy-fedorishin@users.noreply.github.com>
2024-03-18 09:03:25 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,37 @@
-function slugify(title) {
-  title = title.toLowerCase().replace(/\s+/g, '-');
+// function slugify(title) {
+//   title = title.toLowerCase().replace(/\s+/g, '-');
 
-  return `${title}`;
-}
+//   return `${title}`;
+// }
 
-console.log(slugify('Arrays for begginers'));
-console.log(slugify('English for developer'));
-console.log(slugify('Ten secrets of JavaScript'));
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
+// console.log(slugify('Arrays for begginers'));
+// console.log(slugify('English for developer'));
+// console.log(slugify('Ten secrets of JavaScript'));
+// console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
+
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
+
+const ownerName = apartment.owner.name;
+const ownerPhone = apartment.owner.phone;
+const ownerEmail = apartment.owner.email;
+const numberOfTags = apartment.tags.length;
+const firstTag = apartment.tags[0];
+const lastTag = apartment.tags[2];
+console.log(ownerName);
+console.log(ownerPhone);
+console.log(ownerEmail);
+console.log(numberOfTags);
+console.log(firstTag);
+console.log(lastTag);
