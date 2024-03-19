aae0e819ba0d341fb4db9021e789a4dfe5d9eae8
sergiifedorishin <151446351+sergiy-fedorishin@users.noreply.github.com>
2024-03-19 20:29:09 +0200

    task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,17 @@
+function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
+
+  let totalCalories = 0;
+
+  for (const day of days) {
+    totalCalories += day.calories;
+  }
+  const averageCalories = totalCalories / days.length;
+  return averageCalories;
+}
+
 // Напиши функцію calcAverageCalories(days), яка повертає середньодобове значення кількості калорій, які спортсмен споживав
 // протягом тижня.Функція очікує один параметр: days — масив об’єктів.Кожен об’єкт описує день тижня та кількість калорій calories,
 //   спожитих спортсменом, у цей день.Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,23 @@
-function filterArray(numbers, value) {
-  const newNumbers = [];
-  for (numbers of numbers) {
-    if (numbers > value) {
-      newNumbers.push(numbers);
-    }
-  }
-  return newNumbers;
-}
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// Об’єкт profile описує профіль користувача на ігровій платформі.У його властивостях зберігається ім’я профілю username
+// та кількість активних годин playTime, проведених у грі.
+
+// Доповни об’єкт profile методами для роботи з його властивостями.
+// Метод changeUsername(newName) повинен приймати рядок(нове ім’я) в параметр newName та змінювати значення властивості username
+// на нове.Нічого не повертає.
+// Метод updatePlayTime(hours) повинен приймати число(кількість годин) у параметр hours
+// та збільшити на нього значення властивості playTime.Нічого не повертає.
+// Метод getInfo() має повертати рядок формату < Username > has < amount > active hours!, де < Username > — це ім’я профілю,
+//   а < amount > — кількість ігрових годин.
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
