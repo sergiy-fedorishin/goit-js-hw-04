d39e877cddd71643132fac44cb43b2a0b10cab90
sergiifedorishin <151446351+sergiy-fedorishin@users.noreply.github.com>
2024-03-20 08:21:20 +0200

    tyr

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,18 @@
 const profile = {
   username: 'Jacob',
   playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
 };
 
 // Об’єкт profile описує профіль користувача на ігровій платформі.У його властивостях зберігається ім’я профілю username
